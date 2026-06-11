bbf96f56c41f2b37e9c91c98d969c599bbf2c12c
MACTEPCTBO <egornovatsky@mail.ru>
2026-06-11 14:45:57 +0800

    Изменения кода для хостинга

--- static/api.js
+++ static/api.js
@@ -2,7 +2,7 @@
 // Конфигурация API
 const API_CONFIG = {
     //BASE_URL: 'http://localhost:8000',
-    BASE_URL: 'https://one9-vlbb.onrender.com/',
+    BASE_URL: 'https://one9-vlbb.onrender.com',
     AUTH_TYPE: 'basic'
 };
 
